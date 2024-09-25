18362a6dedabafa33c41c47d60b6539881b73f7d
rikachinen <rchinen@paloaltonetworks.com>
2024-09-25 23:37:04 +0900

    fixed code

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -1,3 +1,4 @@
+package com.example;
 package com.example.demo;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-package com.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
